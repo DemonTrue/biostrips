9387124f7ff32bae8ecb8f313838c12d5124af05
DemonTrue <79610682274@yandex.ru>
2023-07-05 14:21:44 +0300

    test deploy 1

--- main.py
+++ main.py
@@ -333,5 +333,5 @@ def pageNotFound(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host="0.0.0.0", port=8080, debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
